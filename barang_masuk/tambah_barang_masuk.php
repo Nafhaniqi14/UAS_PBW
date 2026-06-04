aa7d73e441b85334ee72abd4ba5d265b32ecb68c
SyadetaAbidTaqwaS <abidtaqwa12@gmail.com>
2026-06-04 18:57:13 +0700

    Memperbarui Fitur Barang Masuk

--- barang_masuk/tambah_barang_masuk.php
+++ barang_masuk/tambah_barang_masuk.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['L091n_t0K0']) || $_SESSION['L091n_t0K0'] !== true) {
+if (!isset($_SESSION['login_Un51k4']) || $_SESSION['login_Un51k4'] !== true) {
     header('Location: ../index.php');
     exit;
 }
@@ -37,11 +37,11 @@ if ($result = $conn->query($sqlSupplier)) {
 <body class="admin-dashboard">
 
 <div class="sidebar d-none d-md-block">
-    <?php include '../layout/sidebar.php'; ?>
+    <?php include '../layout/sidebar_sub.php'; ?>
 </div>
 
 <div class="main-wrapper">
-    <?php include '../layout/nav.php'; ?>
+    <?php include '../layout/nav_sub.php'; ?>
 
     <div class="content">
         <div class="container-fluid">
